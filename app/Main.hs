2457b423b9a9596524d32e90c24a4aea4fdc5f6b
Bengt Marten Agren <maraingist@me.com>
2020-01-25 14:22:56 +0800

    Parse pin-mode file to extract alternate-functions.

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,47 @@
-{-# LANGUAGE RecordWildCards, TupleSections #-}
+{-# LANGUAGE RecordWildCards, TupleSections, DuplicateRecordFields #-}
 module Main where
 
 import Text.HTML.TagSoup
 import System.FilePath
 import Data.Monoid
+import Data.Char (isSpace)
+import Data.List (stripPrefix, break)
+import qualified Data.Map.Strict as Map
 
-data PinType = Power | Reset | I_O deriving (Show)
+data PinMode = PinMode
+    { pinName   :: String
+    , signals   :: [AltFun]
+    }
+    deriving (Show)
+
+data AltFun = AltFun
+    { signalName    :: String
+    , alternateFun  :: Int
+    , peripheral    :: String
+    }
+    deriving (Show)
+
+pinModeFromTags :: [Tag String] -> PinMode
+pinModeFromTags (t:ts) = PinMode{..}
+    where pinName = fromAttrib "Name" t
+          signals = map altFun
+                  $ partitions (~=="<PinSignal>")
+                  $ dropWhile (~/="<PinSignal>") ts
+
+altFun :: [Tag String] -> AltFun
+altFun (t:ts) = AltFun{..}
+    where signalName = fromAttrib "Name" t
+          (alternateFun, peripheral) | Just r <- stripPrefix "GPIO_AF" s
+                                     , (u, '_':v) <- break (=='_') r = (read u, v)
+                                     | otherwise = error "failed to extract alternate function"
+                 where s = filter (not . isSpace) $ innerText ts
+
+pinModeMap :: String -> Map.Map String [AltFun]
+pinModeMap xml = Map.fromList [ (pinName, signals) | PinMode{..} <- xs ]
+    where xs = map pinModeFromTags
+             $ partitions (~=="<GPIO_Pin>")
+             $ dropWhile (~/="<GPIO_Pin>")
+             $ parseTags xml
 
 data Pin
     = PowerPin
@@ -45,6 +81,14 @@ mcuXML = "STM32L433R(B-C)Tx"
 
 main :: IO ()
 main = do
+    pmm <- pinModeMap <$> readFile (stm32CubeMX </> dbDir </> modesXML <.> "xml")
+    print pmm
+{-
+    modes <- parseTags <$> readFile (stm32CubeMX </> dbDir </> modesXML <.> "xml")
+    let pins = partitions (~=="<GPIO_Pin>") $ dropWhile (~/="<GPIO_Pin>") modes
+    mapM_ (print . pinModeFromTags) pins
+-}
+
     tags <- parseTags <$> readFile (stm32CubeMX </> dbDir </> mcuXML <.> "xml")
     let pins = partitions (~=="<Pin>") $ dropWhile (~/="<Pin>") tags
     mapM_ (print . pinFromTags) pins
