fcf11ceee61258630cbba78e74449628204eda1d
Bengt Marten Agren <maraingist@me.com>
2020-05-18 09:10:51 +0800

    Handle overlapping registers.

--- src/PrettySVD.hs
+++ src/PrettySVD.hs
@@ -3,6 +3,7 @@ module PrettySVD (prettyPeripheral, peripheralMap) where
 
 import qualified Data.Text as T
 import Data.List (sortOn)
+import Data.List.Extra (nubOn, groupSortOn)
 import Data.Bits (shift)
 import Data.Maybe (isJust)
 import ParseSVD
@@ -20,7 +21,7 @@ prettyPeripheral svd Peripheral{..} =
     , "struct " <> tname (Just svd) name
     , "{"
     ] ++
-    concatMap (register w) (reserve $ sortOn addressOffset registers) ++
+    concatMap (register w) (reserve $ fixupRegisters registers) ++
     concatMap registerFields registers ++
     [ "};"
     ]
@@ -28,6 +29,21 @@ prettyPeripheral svd Peripheral{..} =
 
 tname :: Maybe Text -> Text -> Text
 tname qual name = T.toLower $ maybe "" (<>"_") qual <> name <> "_t"
+
+fixupRegisters :: [Register] -> [Register]
+fixupRegisters = map f . groupSortOn addressOffset
+    where f [x] = x
+          f xs@(x:_) = g x
+              { fields = nubOn (\Field{..} -> name)
+                       $ sortOn (\Field{..} -> name)
+                       $ concatMap fields xs
+              }
+          g r@Register{..}
+              | Just s <- T.stripSuffix "_Output" name
+              = Register{name = s,..}
+              | Just s <- T.stripSuffix "_Input" name
+              = Register{name = s,..}
+              | otherwise = r
  
 reserve :: [Register] -> [(Register, Maybe Int)]
 reserve xs = zip xs $ ys ++ [ Nothing ]
@@ -43,17 +59,21 @@ register w (Register{..}, nextOffset) = T.concat
     , T.replicate (w - T.length name) " "
     , " // " <> maybe "" (\t -> "[" <> t <> "] ") access
     , cleanWords description
-    ] : [ let m = (n - (addressOffset + 4)) `div` 4 in T.concat
-          [ "    "
-          , "reserved_t<"
-          , hex m
-          , "> _"
-          , hex n
-          , ";"
-          ]
-        | Just n <- [ nextOffset ]
-        , n > addressOffset + 4
+    ] : (maybe [] (:[]) $ pad name addressOffset nextOffset)
+
+pad :: Text -> Int -> Maybe Int -> Maybe Text
+pad name addr next
+    | Just n <- next, n < addr + 4
+    = error $ "collission at register " <> T.unpack name
+    | Just n <- next, n > addr + 4 = Just $ T.concat
+        [ "    "
+        , "reserved_t<"
+        , hex $ (n - (addr + 4)) `div` 4
+        , "> _"
+        , hex n
+        , ";"
         ]
+    | otherwise = Nothing
 
 registerFields :: Register -> [Text]
 registerFields Register{..} = "" : rv : xs
