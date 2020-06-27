2a146b0d6ddff88ca59ea3cf49a7e14b3cde922f
Bengt Marten Agren <marangisto@me.com>
2020-06-28 01:46:07 +0800

    Added missing altfun templates.

--- src/PrettyCPP/pins.h
+++ src/PrettyCPP/pins.h
@@ -32,3 +32,31 @@ enum alt_fun_t
     };
 {{/gpio}}
 
+static constexpr gpio_conf_t gpio_conf = mcu_traits<target>::gpio_conf;
+
+template<gpio_conf_t CFG, pin_t PIN, alt_fun_t ALT>
+struct alt_fun_traits
+{
+    static constexpr af_t AF = AF0;
+    static_assert
+        ( always_false_i<PIN>::value
+        , "alternate function not available on this pin!"
+        );
+};
+
+template<bool AVAIL>
+struct available_alt_fun_t
+{
+    using type = alt_fun_t;
+    static assert
+        ( always_false_i<AVAIL>::value
+        , "alternate function not available on pin for mcu"
+        );
+};
+
+template<>
+struct available_alt_fun_t<true>
+{
+    using type = alt_fun_t;
+};
+
