a27c19c6dd9716f3ad4a2e52b2f8cb79146445ae
Bengt Marten Agren <marangisto@me.com>
2020-06-27 19:10:19 +0800

    Map mysterious WB5MM mcu svd.

--- src/FrontEnd/ParseMCU.hs
+++ src/FrontEnd/ParseMCU.hs
@@ -110,6 +110,7 @@ matchSVD svds name = case filter p svds of
     [] | "STM32F437" `isPrefixOf` name -> "STM32F427"
     [] | "STM32F439" `isPrefixOf` name -> "STM32F429"
     [] | "STM32F479" `isPrefixOf` name -> "STM32F469"
+    [] | "STM32WB5M" `isPrefixOf` name -> "STM32WB55" -- FIXME: questionable!
     [] ->  error $ "failed to match svd for " <> unpack name
     [ svd ] -> svd
     xs -> case filter tame xs of
